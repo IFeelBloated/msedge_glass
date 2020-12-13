c887c33d1c0732120c9fa3b87b56abe103c212a6
John Doe <nickgray0@gmail.com>
2020-12-13 10:03:59 -0800

    bug fix

--- Source.cpp
+++ Source.cpp
@@ -5,10 +5,9 @@ auto wmain(int argc, const wchar_t** argv)->int {
     using namespace std::literals;
     auto si = STARTUPINFO{ .cb = sizeof(STARTUPINFO), .dwFlags = STARTF_USESHOWWINDOW, .wShowWindow = SW_HIDE };
     auto pi = PROCESS_INFORMATION{};
-    auto cmd = L"msedge_internal.exe"s;
+    auto cmd = L"msedge_internal.exe --disable-windows10-custom-titlebar"s;
     for (auto x = 1; x < argc; ++x)
         if (argv[x] != L"--disable-windows10-custom-titlebar"sv)
             cmd += L" "s + argv[x];
-    cmd += L" --disable-windows10-custom-titlebar";
     CreateProcess(nullptr, cmd.data(), nullptr, nullptr, false, CREATE_NO_WINDOW, nullptr, nullptr, &si, &pi);
 }
